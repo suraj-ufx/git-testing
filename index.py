cff8cb5b8a4a61c3e6d0f087c0b83fe5cf0d5be6
suraj-ufx <suraj@unifyxperts.com>
2026-01-29 16:57:01 +0530

    solved conflict from cherry pick

--- index.py
+++ index.py
@@ -3,3 +3,5 @@ def test():
     print("test branch hrutik")
     print("stahsing in feature branch")
     print("cherry picking main branch from feature branch")
+    print("cherry picking main branch from feature branch")
+    print("trying cherry pick from the feature branch from main branch")
