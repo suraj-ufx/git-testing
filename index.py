1477083220de70f7a61d5117bc582fca716c3914
suraj-ufx <suraj@unifyxperts.com>
2026-01-29 16:57:01 +0530

    prac

--- index.py
+++ index.py
@@ -4,3 +4,4 @@ def test():
     print("now i will stash this...")
     print("stahsing in feature branch")
     print("cherry picking main branch from feature branch")
+    print("trying cherry pick from the feature branch from main branch")
